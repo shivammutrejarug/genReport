29ad0841ef5d6247d8b73a5bc9947d0cb4b049d5
Alexander Fyodorov <38285390+AlexFyod@users.noreply.github.com>
2020-04-19 21:44:25 +0200

    Moved URL extraction to utils

--- analyzer.py
+++ analyzer.py
@@ -1,9 +1,11 @@
 from JiraAnalyzer.jira_parser import JiraParser
-import re
+import utils
 
-URL_REGEX = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 PROJECTS = ["PDFBOX"]
 
-url_pattern = re.compile(URL_REGEX)
 parser = JiraParser(PROJECTS[0])
 parser.fetch_and_store_comments()
+
+# This is just an example of URL extraction from a string
+example_string = "If you want to reach https://www.wikipedia.org/ from Google, you should enter the following URL: https://www.google.com/search?q=wikipedia&oq=wikipedia&aqs=chrome.0.69i59j0l3j69i65j69i60l3.2620j0j7&sourceid=chrome&ie=UTF-8"
+print(utils.extract_urls_from_string(example_string))
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,14 @@
 import json
+import re
+
+URL_REGEX = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+url_pattern = re.compile(URL_REGEX)
 
 
 def save_as_json(obj: object, path: str):
     with open(path, "w") as file:
         json.dump(obj, file, indent=2)
+
+
+def extract_urls_from_string(string: str):
+    return url_pattern.findall(string)
