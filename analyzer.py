4838e1ef4870fca1df07978b600576271f6a6478
Alexander Fyodorov <alex.fyod23@gmail.com>
2020-04-26 12:04:08 +0200

    Added more projects to parse

--- analyzer.py
+++ analyzer.py
@@ -2,10 +2,11 @@ from jira_analyzer.jira_parser import JiraParser
 import os
 import utils
 
-PROJECTS = ["PDFBOX"]
+PROJECTS = ["PDFBOX", "DERBY", "CASSANDRA"]
 
 args = utils.parse_arguments()
 jira_project = args.jira_project if args.jira_project else PROJECTS[0]
+print("Fetching project {}".format(jira_project))
 parser = JiraParser(jira_project)
 
 parser.fetch_and_save_comments()
