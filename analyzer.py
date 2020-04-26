b1d8ab23705acf6da79b9a953cae11a001a17bd9
Alexander Fyodorov <alex.fyod23@gmail.com>
2020-04-26 12:15:30 +0200

    Added more projects

--- analyzer.py
+++ analyzer.py
@@ -2,13 +2,29 @@ from jira_analyzer.jira_parser import JiraParser
 import os
 import utils
 
-PROJECTS = ["PDFBOX", "DERBY", "CASSANDRA"]
+PROJECTS = ["PDFBOX",
+            "DERBY",
+            "CASSANDRA",
+            "YARN",
+            "HDFS",
+            "HADOOP",
+            "MAPREDUCE",
+            "ZOOKEEPER",
+            "CONNECTORS",  # ManifoldCF https://issues.apache.org/jira/projects/CONNECTORS/summary
+            "BIGTOP",
+            "OFBIZ",
+            "DIRSTUDIO",  # Directory Studio https://issues.apache.org/jira/projects/DIRSTUDIO/summary
+            "DIRMINA",  # MINA https://issues.apache.org/jira/projects/DIRMINA/summary
+            "CAMEL",  # Camel https://issues.apache.org/jira/projects/CAMEL/summary
+            "AXIS2"  # Axis2 https://issues.apache.org/jira/projects/AXIS2/summary
+            ]
 
 args = utils.parse_arguments()
-jira_project = args.jira_project if args.jira_project else PROJECTS[0]
-print("Fetching project {}".format(jira_project))
-parser = JiraParser(jira_project)
+projects = [args.jira_project] if args.jira_project else PROJECTS
+for project in projects:
+    print("Fetching project {}".format(project))
+    parser = JiraParser(project)
 
-parser.fetch_and_save_comments()
-utils.extract_urls(input_directory=os.path.join("Projects", jira_project, "Issues"),
-                   output_directory=os.path.join("Projects", jira_project, "URLs"))
+    parser.fetch_and_save_comments()
+    utils.extract_urls(input_directory=os.path.join("Projects", project, "Issues"),
+                       output_directory=os.path.join("Projects", project, "URLs"))
