402fcdb4bc683937ca6e08f16a4c2a93f57bce6e
Alexander Fyodorov <alex.fyod23@gmail.com>
2020-04-26 12:53:00 +0200

    Added more projects

--- analyzer.py
+++ analyzer.py
@@ -2,22 +2,33 @@ from jira_analyzer.jira_parser import JiraParser
 import os
 import utils
 
-PROJECTS = ["PDFBOX",
-            "DERBY",
-            "CASSANDRA",
-            "YARN",
-            "HDFS",
-            "HADOOP",
-            "MAPREDUCE",
-            "ZOOKEEPER",
-            "CONNECTORS",  # ManifoldCF https://issues.apache.org/jira/projects/CONNECTORS/summary
-            "BIGTOP",
-            "OFBIZ",
-            "DIRSTUDIO",  # Directory Studio https://issues.apache.org/jira/projects/DIRSTUDIO/summary
-            "DIRMINA",  # MINA https://issues.apache.org/jira/projects/DIRMINA/summary
-            "CAMEL",  # Camel https://issues.apache.org/jira/projects/CAMEL/summary
-            "AXIS2"  # Axis2 https://issues.apache.org/jira/projects/AXIS2/summary
-            ]
+PROJECTS = [
+    "PDFBOX",
+    "DERBY",
+    "CASSANDRA",
+    "YARN",
+    "HDFS",
+    "HADOOP",
+    "MAPREDUCE",
+    "ZOOKEEPER",
+    "CONNECTORS",  # ManifoldCF https://issues.apache.org/jira/projects/CONNECTORS/summary
+    "BIGTOP",
+    "OFBIZ",
+    "DIRSTUDIO",  # Directory Studio https://issues.apache.org/jira/projects/DIRSTUDIO/summary
+    "DIRMINA",  # MINA https://issues.apache.org/jira/projects/DIRMINA/summary
+    "CAMEL",  # Camel https://issues.apache.org/jira/projects/CAMEL/summary
+    "AXIS2"  # Axis2 https://issues.apache.org/jira/projects/AXIS2/summary
+]
+
+PROJECTS_WITH_QA_BOTS = [
+    "HBASE",  # HBase https://issues.apache.org/jira/projects/HBASE/summary
+    "HIVE",  # Hive https://issues.apache.org/jira/projects/HIVE/summary
+]
+
+PROJECTS_WITH_PULL_REQUESTS = [
+    "TAJO",  # Tajo https://issues.apache.org/jira/projects/TAJO/summary
+    "NUTCH"  # Nutch https://issues.apache.org/jira/projects/NUTCH/summary
+]
 
 args = utils.parse_arguments()
 projects = [args.jira_project] if args.jira_project else PROJECTS
