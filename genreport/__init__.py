1f20fc110a9f5f9e0bb5bfcb26e69709020c7e56
Alexander Fyodorov <38285390+AlexFyod@users.noreply.github.com>
2020-07-05 04:29:58 +0200

    Implemented report generation without comments and expanded references

--- genreport/__init__.py
+++ genreport/__init__.py
@@ -1,18 +1,41 @@
 from jira import Issue
-from pylatex import Document, Section, Subsection, Package, Hyperref
+from pylatex import Document, Section, Subsection, Package, Hyperref, Command, Enumerate
 from pylatex.utils import escape_latex, NoEscape
 import utils
 import os
 
 
 class ReportGenerator:
-    def __init__(self, project: str, issue: Issue, github_repository: str = None):
+    def __init__(self, project, issue, github_repository: str = None):
         self.project = project
         self.issue = issue
         self.github_repository = github_repository
         self.data = self.__load_issue()
-        self.doc = Document()
-        self.doc.packages.append(Package("a4wide"))
+        self.doc = Document(document_options="article")
+        packages = self.doc.packages
+        packages.append(Package("a4wide"))
+        packages.append(Package("listings"))
+        packages.append(Package("xcolor"))
+        packages.append(Package("courier"))
+        packages.append(Package("tabularx"))
+        packages.append(Package("hyperref"))
+
+        preamble = self.doc.preamble
+        preamble.append(Command("title", self.data["issue_key"]))
+        preamble.append(Command("author", self.data["author"]))
+        preamble.append(Command("date", self.data["created"].split("T")[0]))
+        preamble.append(NoEscape(r"\lstset{tabsize = 4,"
+                                          r"showstringspaces = false,"
+                                          r"numbers = left,"
+                                          r"commentstyle = \color{darkgreen} \ttfamily,"
+                                          r"keywordstyle = \color{blue} \ttfamily,"
+                                          r"stringstyle = \color{red} \ttfamily,"
+                                          r"rulecolor = \color{black} \ttfamily,"
+                                          r"basicstyle = \footnotesize \ttfamily,"
+                                          r"frame = single,"
+                                          r"breaklines = true,"
+                                          r"numberstyle = \tiny}"))
+        preamble.append(NoEscape(r"\definecolor{darkgreen}{rgb}{0,0.6,0}"))
 
         self.other_issues = []
 
@@ -28,9 +51,19 @@ class ReportGenerator:
     def generate_report(self):
         doc = self.doc
         data = self.data
-        with doc.create(Section(self.issue)):
-            doc.append("Status: {}\n".format(data["status"]))
-            doc.append("Created: {}\n".format(data["created"]))
-            doc.append("Updated: {}\n".format(data["updated"]))
-            with doc.create(Subsection("Attachments")):
-                doc.append()
+        doc.append(NoEscape(r"\maketitle"))
+
+        with doc.create(Section("Summary")):
+            doc.append(NoEscape(data["summary"]))
+
+        with doc.create(Section("Description")):
+            description = utils.atlassian_code_format_to_listing(data["description"])
+            doc.append(NoEscape(description))
+
+        with doc.create(Section("Attachments")):
+            attachments = data["attachments"]
+            with doc.create(Enumerate()) as enum:
+                for attachment in attachments:
+                    enum.add_item(self.__hyperlink(attachment["content"], attachment["filename"]))
+
+        doc.generate_pdf(data["issue_key"], clean_tex=True)
