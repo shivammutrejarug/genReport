5eb05a569a02d7eefaa08f4982c35945c13ccf6b
Alexander Fyodorov <alex.fyod23@gmail.com>
2020-07-12 02:04:01 +0200

    Added mailing list keys and removed useless function

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,7 +2,6 @@ import argparse
 import errno
 import json
 import os
-from typing import Set
 from .ref_regex import *
 
 
@@ -52,44 +51,17 @@ def filter_pdf_document_urls(urls: Set[str]) -> Set[str]:
     return set([url for url in urls if url.endswith(".pdf")])
 
 
-def filter_mailing_list_urls(urls: Set[str], mailinglist_keys=None) -> Set[str]:
+def filter_mailing_list_urls(urls: Set[str], mailing_list_keys=None) -> Set[str]:
     """
     Filter URLs leading to mailing lists. This is a very rough implementation and should definitely be improved.
     :param urls: List of URLs to filter mailing lists from
-    :param mailinglist_keys: If the URL is a mailing list, any entry from this list should be present in the URL.
-    Otherwise, it searches for the word "mail" only
+    :param mailing_list_keys: If the URL is a mailing list, any entry from this list should be present in the URL.
+    Otherwise, it checks whether the url contains "mail-archives" or "markmail".
     :return: List of mailing list URLs
     """
-    if not mailinglist_keys:
-        mailinglist_keys = ["mail"]
-    return set([url for url in urls if any(key in url for key in mailinglist_keys)])
-
-
-def extract_and_save_urls_from_directory(input_directory: str, output_directory: str) -> None:
-    """
-    Extract URLs from each file in input_directory and
-    save them in appropriate files in .txt format in output_directory.
-    :param input_directory: Directory to read files from
-    :param output_directory: Directory to write files to
-    :return: None
-    """
-    if not os.path.exists(input_directory):
-        print("Input directory does not exist!")
-        return
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
-
-    input_files = os.listdir(input_directory)
-
-    for input_file in input_files:
-        input_path = os.path.join(input_directory, input_file)
-        with open(input_path, "r") as file:
-            urls = extract_urls(file.read())
-
-        output_file = os.path.splitext(input_file)[0] + ".txt"
-        output_path = os.path.join(output_directory, output_file)
-        with open(output_path, 'w') as file:
-            file.write('\n'.join(urls))
+    if not mailing_list_keys:
+        mailing_list_keys = ["mail-archives", "markmail"]
+    return set([url for url in urls if any(key in url for key in mailing_list_keys)])
 
 
 def atlassian_code_format_to_listing(string: str) -> str:
