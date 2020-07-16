53ae4c1bc9b6e6c2b4d0a2707a886e846e0e72d8
Alexander Fyodorov <alex.fyod23@gmail.com>
2020-07-16 04:34:53 +0200

    Implemented code listing escaping

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,8 @@ import errno
 import json
 import os
 from typing import Tuple
+from pylatex.utils import escape_latex, NoEscape
+
 from .ref_regex import *
 
 
@@ -83,21 +85,35 @@ def filter_mailing_list_urls(urls: Set[str], mailing_list_keys=None) -> Set[str]
     return set([url for url in urls if any(key in url for key in mailing_list_keys)])
 
 
-def atlassian_code_format_to_listing(string: str) -> str:
-    string = string.replace(r"{code:java}", r"\begin{lstlisting}[language=Java]")
-    string = string.replace(r"{code}", r"\end{lstlisting}")
-
-    while string.find(r"{noformat}") != -1:
-        string = string.replace(r"{noformat}", r"\begin{lstlisting}", 1)
-        string = string.replace(r"{noformat}", r"\end{lstlisting}", 1)
-
+def noformat_to_latex(string: str) -> str:
+    """
+    Convert {noformat} blocks to LaTeX verbatim with line breaking.
+    :param string: String to replace noformat at. String should be already parsed by escape_latex
+    :return: Formatted string
+    """
+    while string.find(r"\{noformat\}") != -1:
+        string = string.replace(r"\{noformat\}", r"\begin{spverbatim}", 1)
+        string = string.replace(r"\{noformat\}", r"\end{spverbatim}\ ", 1)
     return string
 
 
-def extract_code_listings(string: str) -> Tuple[str, List[Tuple[str, str]]]:
+def extract_code_listings(string: str, to_latex: bool = True) -> Tuple[str, List[Tuple[str, str]]]:
+    """
+    Given a string with Atlassian code listings, replaces them with flags and returns a tuple of a string with flags
+    and a list of code listings with corresponding flags. This is primarily used to avoid escaping LaTeX characters
+    inside listings.
+    :param string: String with Atlassian code listings
+    :param to_latex: Whether to convert listings to LaTeX format
+    :return: Tuple containing two values:
+    1. String with all code listings replaced with flags of the form <<!PDFGEN123!>>, where 123 is the serial number
+    of a listing
+    2. List of tuples, each of which contains two values:
+        2.1 Flag
+        2.2 Content that is intended to replace the flag in the string
+    """
     listings = []
     listing_index = 1
-    pattern = re.compile(r"({(code:(.*?))|(code)})(.*?){code}")
+    pattern = re.compile(r"(({code:((?s).*?)})|({code}))((?s).*?){code}")
 
     while True:
         listing = pattern.search(string)
@@ -106,7 +122,36 @@ def extract_code_listings(string: str) -> Tuple[str, List[Tuple[str, str]]]:
         content = listing.group(0)
         key = "<<!PDFGEN{}!>>".format(listing_index)
         listing_index += 1
-        listings.append((key, content))
         string = string.replace(content, key)
+        if to_latex:
+            language = re.search(r"{code:((?s).*?)}", content)
+            if language:
+                section = language.group(0)
+                language = language.group(1)
+                content = content.replace(section, r"\begin{lstlisting}[language=" + language + "]", 1)
+            else:
+                content = content.replace(r"{code}", r"\begin{lstlisting}", 1)
+            content = content.replace(r"{code}", r"\end{lstlisting}\ ", 1)
+        if len(content) > 400:
+            # If the string length is too, LaTeX throws an error "Dimension too large".
+            # Unfortunately, I couldn't find what is the max dimension, so let's say that typical
+            # line never exceeds 400 characters, and so, after each 400 characters, a newline character is inserted.
+            content = '\n'.join(content[i:i + 400] for i in range(0, len(content), 400))
+        listings.append((key, content))
     return string, listings
 
+
+def escape_with_listings(string: str):
+    """
+    Escape LaTeX characters except code listings. All Atlassian code listings are converted to the corresponding
+    LaTeX ones.
+    :param string: String containing text without escaping and with Atlassian code listings
+    :return: Formatted string
+    """
+    string, listings = extract_code_listings(string)
+    string = escape_latex(string)
+    for listing in listings:
+        key, content = listing
+        string = string.replace(key, content, 1)
+    string = noformat_to_latex(string)
+    return NoEscape(string)
