527fd5a06414cc9f7852aaa783e275da6cb7dd53
Alexander Fyodorov <alex.fyod23@gmail.com>
2020-07-17 03:38:18 +0200

    Added documentation + refactoring

--- github_fetcher/__init__.py
+++ github_fetcher/__init__.py
@@ -5,22 +5,49 @@ from typing import List
 
 import utils
 
+DATE_FORMAT = "%Y-%m-%d"
+
 
 class GitHubFetcher:
     def __init__(self, project: str, repo_name: str):
         self.project = project
         self.github = Github("AlexFyod", "NormalFortExpress23yZ4R3R}{7if2")
         self.repo = self.github.get_repo(repo_name)
-        self.savedir = os.path.join("Projects", self.project, "Commits")
+        self.savedir_commits = os.path.join("Projects", self.project, "Commits")
+        self.savedir_pull_requests = os.path.join("Projects", self.project, "PullRequests")
 
-    def get_commits(self, issue_key: str = None):
-        path = os.path.join(self.savedir, issue_key + ".json")
+    @staticmethod
+    def __save_json(json_list: List[dict], directory: str, issue_key: str = None) -> None:
+        """
+        Save target list of dictionaries to the desired directory in a file in JSON format.
+        If issue_key is specified, then the file is name "<issue_key>.json", otherwise - "all.json".
+        :param json_list: List of dictionaries to save as JSON file
+        :param directory: Directory where to save the file
+        :param issue_key: Target issue key
+        :return: None
+        """
+        utils.create_dir_if_necessary(directory)
+        if issue_key:
+            filename = issue_key + ".json"
+        else:
+            filename = "all.json"
+        path = os.path.join(directory, filename)
+        utils.save_as_json(json_list, path)
+
+    def get_commits(self, issue_key: str = None) -> List[dict]:
+        """
+        Get list of dictionaries representing commits for the desired issue. If issue_key is not specified, then
+        all commits are retrieved.
+        :param issue_key: Target issue key
+        :return: List of dictionaries representing commits
+        """
+        if issue_key:
+            filename = issue_key + ".json"
+        else:
+            filename = "all.json"
+        path = os.path.join(self.savedir_commits, filename)
         if not os.path.isfile(path):
-            path = os.path.join(self.savedir, "all.json")
-            if not os.path.isfile(path):
-                commits = self.fetch_commits()
-            else:
-                commits = utils.load_json(path)
+            commits = self.fetch_commits(issue_key)
         else:
             commits = utils.load_json(path)
         if issue_key:
@@ -33,7 +60,15 @@ class GitHubFetcher:
             )
         return commits
 
-    def fetch_commits(self, issue_key: str = None, save: bool = True):
+    def fetch_commits(self, issue_key: str = None, save: bool = True) -> List[dict]:
+        """
+        Fetch and parse all commits for the target project. If issue_key is specified, then only PRs targeting the issue
+        are retrieved. Targeting is determined by whether the commit message starts with issue_key + ':' since it is the
+        standard convention for commit description.
+        :param issue_key: Target issue key
+        :param save: Whether to save commits to a file
+        :return: List of dictionaries representing commits
+        """
         commits_raw = self.repo.get_commits()
         if issue_key:
             prefix = issue_key + ":"
@@ -50,7 +85,7 @@ class GitHubFetcher:
             commit["sha"] = sha
             commit["short_sha"] = sha[:7]
             commit["author"] = commit_raw.commit.author.name
-            commit["date"] = commit_raw.commit.author.date.date().strftime("%Y-%m-%d")
+            commit["date"] = commit_raw.commit.author.date.date().strftime(DATE_FORMAT)
             commit["message"] = commit_raw.commit.message
             commits.append(commit)
 
@@ -58,11 +93,90 @@ class GitHubFetcher:
             self.__save_commits(commits, issue_key)
         return commits
 
-    def __save_commits(self, commits: List[dict], issue_key: str = None):
-        utils.create_dir_if_necessary(self.savedir)
+    def __save_commits(self, commits: List[dict], issue_key: str = None) -> None:
+        """
+        Save commits to a file. If issue_key is not specified, then commits are saved to "all.json" file.
+        :param commits: List of dictionaries representing commits
+        :param issue_key: Target issue key
+        :return: None
+        """
+        self.__save_json(commits, self.savedir_commits, issue_key)
+
+    def get_pull_requests(self, issue_key: str = None) -> List[dict]:
+        """
+        Get list of dictionaries representing pull requests for the desired issue. If issue_key is not specified, then
+        all pull requests are retrieved.
+        :param issue_key: Target issue key
+        :return: List of dictionaries representing pull requests
+        """
         if issue_key:
             filename = issue_key + ".json"
         else:
             filename = "all.json"
-        path = os.path.join(self.savedir, filename)
-        utils.save_as_json(commits, path)
+        path = os.path.join(self.savedir_pull_requests, filename)
+        if not os.path.isfile(path):
+            pull_requests = self.fetch_pull_requests(issue_key)
+        else:
+            pull_requests = utils.load_json(path)
+        if issue_key and filename == "all.json":
+            pull_requests = list(
+                filter(
+                    lambda pr: issue_key in utils.extract_issues(pr["title"], self.project) or
+                               issue_key in utils.extract_issues(pr["body"], self.project),
+                    pull_requests
+                )
+            )
+        return pull_requests
+
+    def fetch_pull_requests(self, issue_key: str = None, save: bool = True) -> List[dict]:
+        """
+        Fetch and parse all pull requests for the target project, both closed and opened. If issue_key is specified,
+        then only PRs targeting the issue are retrieved. Targeting is determined by the presence of issue_key
+        in PR title or its body.
+        :param issue_key: Target issue key
+        :param save: Whether to save pull requests to a file
+        :return: List of dictionaries representing pull requests
+        """
+        pull_requests_raw = self.repo.get_pulls(state="all")
+        if issue_key:
+            pull_requests_raw = list(
+                filter(
+                    lambda pr: issue_key in utils.extract_issues(pr.title, self.project) or
+                               issue_key in utils.extract_issues(pr.body, self.project),
+                    pull_requests_raw
+                )
+            )
+        pull_requests = []
+        for pr_raw in pull_requests_raw:
+            pr = dict()
+            pr["number"] = pr_raw.number
+            pr["title"] = pr_raw.title
+            pr["author"] = pr_raw.user.login
+            pr["status"] = pr_raw.state
+            pr["date"] = pr_raw.created_at.strftime(DATE_FORMAT)
+            pr["body"] = pr_raw.body
+
+            # Now let's fetch comments
+            pr["comments"] = []
+            pr_comments = pr["comments"]
+            for comment in pr_raw.get_issue_comments():
+                comment_dict = dict()
+                comment_dict["author"] = comment.user.login
+                comment_dict["date"] = comment.created_at.strftime(DATE_FORMAT)
+                comment_dict["body"] = comment.body
+                pr_comments.append(comment_dict)
+
+            pull_requests.append(pr)
+
+        if save:
+            self.__save_pull_requests(pull_requests, issue_key)
+        return pull_requests
+
+    def __save_pull_requests(self, pull_requests: List[dict], issue_key: str = None) -> None:
+        """
+        Save pull requests to a file. If issue_key is not specified, then pull requests are saved to "all.json" file.
+        :param pull_requests: List of dictionaries representing pull requests
+        :param issue_key: Target issue key
+        :return: None
+        """
+        self.__save_json(pull_requests, self.savedir_pull_requests, issue_key)
